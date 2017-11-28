81ee602aa99153e0f5c90273fd00584d0fba4d81
Johnny Capra <johnnycapra@Johnnys-MacBook-Pro.local>
2017-11-28 15:42:39 -0700

    project submit

---
+++ App.js
@@ -0,0 +1,77 @@
+import Expo, { Notifications } from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View, ALert } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import store from './store';
+import AuthScreen from './screens/AuthScreen'
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import registerForNotifications from './services/push_notifications';
+
+export default class App extends React.Component {
+  
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+    // above is same as const text = notification.data.text
+      if (origin === 'received') {
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'Ok. '}]
+        );
+      }
+    });
+  }
+
+  render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        }, {
+          tabBarPosition: 'bottom',
+          tabBarOptions :{
+            labelStyle: { fontSize: 12 }
+          }
+        })
+      }
+    }, {
+      navigationOptions: {
+        tabBarVisible: false
+      },
+      lazy: true
+    });
+
+    return (
+      <Provider store={store}>
+          <MainNavigator />
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+Expo.registerRootComponent(App);
---
+++ actions/types.js
@@ -0,0 +1,5 @@
+export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
+export const FACEBOOK_LOGIN_FAIL = 'facebook-login-fail';
+export const LIKE_JOB = 'like_job';
+export const CLEAR_LIKED_JOBS = 'clear_liked_jobs';
+export const FETCH_JOBS = 'fetch_jobs';
